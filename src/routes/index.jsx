96e80c9120a9d442cac7c40e0fb383ab19d90d7d
Arman Mia <mdarman365506@gmail.com>
2025-09-22 14:31:35 +0600

    electrical/advertising-ai page done

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -80,7 +80,7 @@ export const router = createBrowserRouter([
 
       // electrical page
       {
-        path: "/c",
+        path: "/electrical",
         Component: Electrical,
       },
       {
